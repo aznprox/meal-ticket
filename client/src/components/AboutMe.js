f0b8ae216d71230c88dec71b4dce76f95eee9c36
unknown <aznprox@hotmail.com>
2018-02-08 09:40:57 -0500

    innital routes created

---
+++ client/src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AboutMe extends Component {
+    render() {
+        return (
+            <div>
+            <h1>This is the About Me Page</h1>    
+            </div>
+        );
+    }
+}
+
+export default AboutMe;
---
+++ client/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is my Nav Bar</h1>
+            </div>
+        );
+    }
+}
+
+export default Nav;
